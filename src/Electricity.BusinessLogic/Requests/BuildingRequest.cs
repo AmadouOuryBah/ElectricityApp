cfc06edaf4559a0d2def4d78985bfea09605c89e
unknown <Amadourybah3732@gmail.com>
2023-06-03 16:36:40 +0200

    BuildingRequest Model

--- src/Electricity.BusinessLogic/Requests/BuildingRequest.cs
+++ src/Electricity.BusinessLogic/Requests/BuildingRequest.cs
@@ -1,9 +1,4 @@
-﻿using Electricity.DataAccess.Entities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
 
 namespace Electricity.BusinessLogic.Requests
 {
